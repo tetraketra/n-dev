126f3b6a8a0d3f18ceebf564662c62b84fd609cc
Tetra Silverberg <tetra.silv@gmail.com>
2024-03-12 13:55:15 -0400

    skeleton

---
+++ src/hsv.hpp
@@ -0,0 +1,57 @@
+#ifndef HSV_HPP
+#define HSV_HPP
+
+#include <SmartMatrix.h>
+
+typedef struct hsv24{
+    unsigned char h; /* 0-255 not 0-360 */
+    unsigned char s;
+    unsigned char v;
+} hsv24;
+
+rgb24 HsvToRgb(hsv24 hsv)
+{
+    rgb24 rgb;
+    unsigned char region, remainder, p, q, t;
+    
+    if (hsv.s == 0)
+    {
+        rgb.red = hsv.v;
+        rgb.green = hsv.v;
+        rgb.blue = hsv.v;
+        return rgb;
+    }
+    
+    region = hsv.h / 43;
+    remainder = (hsv.h - (region * 43)) * 6; 
+    
+    p = (hsv.v * (255 - hsv.s)) >> 8;
+    q = (hsv.v * (255 - ((hsv.s * remainder) >> 8))) >> 8;
+    t = (hsv.v * (255 - ((hsv.s * (255 - remainder)) >> 8))) >> 8;
+    
+    switch (region)
+    {
+        case 0:
+            rgb.red = hsv.v; rgb.green = t; rgb.blue = p;
+            break;
+        case 1:
+            rgb.red = q; rgb.green = hsv.v; rgb.blue = p;
+            break;
+        case 2:
+            rgb.red = p; rgb.green = hsv.v; rgb.blue = t;
+            break;
+        case 3:
+            rgb.red = p; rgb.green = q; rgb.blue = hsv.v;
+            break;
+        case 4:
+            rgb.red = t; rgb.green = p; rgb.blue = hsv.v;
+            break;
+        default:
+            rgb.red = hsv.v; rgb.green = p; rgb.blue = q;
+            break;
+    }
+    
+    return rgb;
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <MatrixHardware_Teensy4_ShieldV5.h>
 #include <SmartMatrix.h>
 
+// #include <random>
+
+#include "hsv.hpp"
+
 #define COLOR_DEPTH 24               // Choose the color depth used for storing pixels in the layers: 24 or 48 (24 is good for most sketches - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24)
 const uint16_t kMatrixWidth   = 128; // Set to the width of your display, must be a multiple of 8
 const uint16_t kMatrixHeight  = 32;  // Set to the height of your display
@@ -12,37 +16,70 @@ const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
 const uint8_t kScrollingLayerOptions = (SM_SCROLLING_OPTIONS_NONE);
 const uint8_t kIndexedLayerOptions = (SM_INDEXED_OPTIONS_NONE);
 
+const int defaultBrightness = (100*255)/100;        // full (100%) brightness, adjust the 100* for brightness
+const int defaultScrollOffset = 6;
+const rgb24 defaultBackgroundColor = {0, 0, 0};
+
 SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
 SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
 SMARTMATRIX_ALLOCATE_INDEXED_LAYER(indexedLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kIndexedLayerOptions);
 
-#include "gimpbitmap.h"
+typedef enum protos_state_os_e {
+    __PSTATE_SEC_POWER_BEG,
+    PSTATE_OFF,
+    PSTATE_BOOTING,
+    PSTATE_SHUTTINGDOWN,
+    __PSTATE_SEC_POWER_END,
 
-const int defaultBrightness = (100*255)/100;        // full (100%) brightness
-//const int defaultBrightness = (15*255)/100;       // dim: 15% brightness
-const int defaultScrollOffset = 6;
-const rgb24 defaultBackgroundColor = {0x40, 0, 0};
+    __PSTATE_SEC_CELLAUTO_BEG,
+    PSTATE_LIFE, /* game of life */
+    PSTATE_RPSLS, /* rock paper scissors lizard spock */
+    PSTATE_SAND, /* sand pile */
+    __PSTATE_SEC_CELLAUTO_END
+} protos_state_os_e;
 
-// Teensy 3.0 has the LED on pin 13
-const int ledPin = 13;
+typedef struct protos_state_anim_s {
+    bool     is_animating;
+    bool     is_paused;
+    uint16_t frame;
+    float    progress; /* 0.0 means first frame (if playing), 1.0 means 100% completed */
+} protos_state_anim_s;
 
-void drawBitmap(int16_t x, int16_t y, const gimp32x32bitmap* bitmap) {
-  for(unsigned int i=0; i < bitmap->height; i++) {
-    for(unsigned int j=0; j < bitmap->width; j++) {
-      rgb24 pixel = { bitmap->pixel_data[(i*bitmap->width + j)*3 + 0],
-                      bitmap->pixel_data[(i*bitmap->width + j)*3 + 1],
-                      bitmap->pixel_data[(i*bitmap->width + j)*3 + 2] };
+class ProtosState {
+    public:
+        protos_state_os_e state_os;
+        protos_state_anim_s state_anim;
 
-      backgroundLayer.drawPixel(x + j, y + i, pixel);
-    }
-  }
-}
+    public:
+        ProtosState() : state_os(PSTATE_OFF), state_anim() {}
+
+        void fetch_inputs(void) {
+            /* ... */
+        }
+
+        void update_state(void) {
+            switch (this->state_os) {
+                case PSTATE_OFF:
+                    /* do nothing */
+                    break;
+                case PSTATE_BOOTING:
+                    /* TODO: play animation */
+                    break;
+                case PSTATE_SHUTTINGDOWN:
+                    /* TODO: play animation */
+                    break;
+                default:
+                    break;
+            }
+        }
+};
+
+ProtosState protos_state = ProtosState();
+
+/* --- --- --- --- */
 
 void setup() {
-    // initialize the digital pin as an output.
-    pinMode(ledPin, OUTPUT);
-
-    Serial.begin(115200);
+    pinMode(13, OUTPUT);
 
     matrix.addLayer(&backgroundLayer); 
     matrix.addLayer(&indexedLayer); 
@@ -53,120 +90,113 @@ void setup() {
     backgroundLayer.enableColorCorrection(true);
 }
 
-
-/* IDEAS
- - MIRROR MODE
-  - you can do whatever on the full canvas, but after all your drawing, half the canvas gets mirrored onto the other half
- - SPLINES https://github.com/ejmahler/SplineLibrary
-  - one spline equals one curve or hollow shape. thickness var when drawing
-  - draw by sampling the spline a number of times proportional to the arc length (that's an existing util function), drawing the corresponding pixel (might redraw)
-  - optionally floodfill for looping splines? draw spline, fill in space? floodfill def needs to be a draw-buffer operation
-  - animate by moving spline knots? You could spline to interpolate between keyframes for knots positions lmao. Double spline ^^
- - PIXEL CELLULAR AUTOMATA
-  - falling sand
-  - water sloshing
-  - rock paper scissors
-  - target gravity (pixels gravitate to or around a point)
- - TRANSITIONS
-  - stop drawing splines, convert existing buffer to cellular automata, gravity/flow down or something
-*/
 void loop() {
-    int i;
-    unsigned long currentMillis;
-
-    // clear screen
     backgroundLayer.fillScreen(defaultBackgroundColor);
+
+    protos_state.fetch_inputs();
+    protos_state.update_state();
+
     backgroundLayer.swapBuffers();
-
-    const int delayBetweenShapes = 250;
-
-    for (i = 0; i < 5000; i += delayBetweenShapes) {
-        // draw for 100ms, then update frame, repeat
-        currentMillis = millis();
-        int x0, y0, x1, y1, x2, y2, radius, radius2;
-        // x0,y0 pair is always on the screen
-        x0 = random(matrix.getScreenWidth());
-        y0 = random(matrix.getScreenHeight());
-
-        x1 = random(matrix.getScreenWidth());
-        y1 = random(matrix.getScreenHeight());
-        // x2,y2 pair is on screen;
-        x2 = random(matrix.getScreenWidth());
-        y2 = random(matrix.getScreenHeight());
-
-        // radius is positive, up to screen width size
-        radius = random(matrix.getScreenWidth());
-        radius2 = random(matrix.getScreenWidth());
-
-        rgb24 fillColor = {(uint8_t)random(192), (uint8_t)random(192), (uint8_t)random(192)};
-        rgb24 outlineColor = {(uint8_t)random(192), (uint8_t)random(192), (uint8_t)random(192)};
-
-        switch (random(15)) {
-        case 0:
-            backgroundLayer.drawPixel(x0, y0, outlineColor);
-            // backgroundLayer.readPixel(x0, y0);
-            break;
-
-        case 1:
-            backgroundLayer.drawLine(x0, y0, x1, y1, outlineColor);
-            break;
-
-        case 2:
-            backgroundLayer.drawCircle(x0, y0, radius, outlineColor);
-            break;
-
-        case 3:
-            backgroundLayer.drawTriangle(x0, y0, x1, y1, x2, y2, outlineColor);
-            break;
-
-        case 4:
-            backgroundLayer.drawRectangle(x0, y0, x1, y1, outlineColor);
-            break;
-
-        case 5:
-            backgroundLayer.drawRoundRectangle(x0, y0, x1, y1, radius, outlineColor);
-            break;
-
-        case 6:
-            backgroundLayer.fillCircle(x0, y0, radius, fillColor);
-            break;
-
-        case 7:
-            backgroundLayer.fillTriangle(x0, y0, x1, y1, x2, y2, fillColor);
-            break;
-
-        case 8:
-            backgroundLayer.fillRectangle(x0, y0, x1, y1, fillColor);
-            break;
-
-        case 9:
-            backgroundLayer.fillRoundRectangle(x0, y0, x1, y1, radius, fillColor);
-            break;
-
-        case 10:
-            backgroundLayer.fillCircle(x0, y0, radius, outlineColor, fillColor);
-            break;
-
-        case 11:
-            backgroundLayer.fillTriangle(x0, y0, x1, y1, x2, y2, outlineColor, fillColor);
-            break;
-
-        case 12:
-            backgroundLayer.fillRectangle(x0, y0, x1, y1, outlineColor, fillColor);
-            break;
-
-        case 13:
-            backgroundLayer.fillRoundRectangle(x0, y0, x1, y1, radius, outlineColor, fillColor);
-            break;
-
-        case 14:
-            backgroundLayer.drawEllipse(x0, y0, radius, radius2, outlineColor);
-
-        default:
-            break;
-        }
-        backgroundLayer.swapBuffers();
-        //backgroundLayer.fillScreen({0,0,0});
-        while (millis() < currentMillis + delayBetweenShapes);
-    }
 }
+
+
+// void loop() {
+//     int i;
+//     unsigned long currentMillis;
+
+//     // clear screen
+//     backgroundLayer.fillScreen(defaultBackgroundColor);
+//     backgroundLayer.swapBuffers();
+
+//     const int delayBetweenShapes = 250;
+
+//     for (i = 0; i < 5000; i += delayBetweenShapes) {
+//         // draw for 100ms, then update frame, repeat
+//         currentMillis = millis();
+//         int x0, y0, x1, y1, x2, y2, radius, radius2;
+//         // x0,y0 pair is always on the screen
+//         x0 = random(matrix.getScreenWidth());
+//         y0 = random(matrix.getScreenHeight());
+
+//         x1 = random(matrix.getScreenWidth());
+//         y1 = random(matrix.getScreenHeight());
+//         // x2,y2 pair is on screen;
+//         x2 = random(matrix.getScreenWidth());
+//         y2 = random(matrix.getScreenHeight());
+
+//         // radius is positive, up to screen width size
+//         radius = random(matrix.getScreenWidth());
+//         radius2 = random(matrix.getScreenWidth());
+
+//         rgb24 fillColor = {(uint8_t)random(192), (uint8_t)random(192), (uint8_t)random(192)};
+//         rgb24 outlineColor = {(uint8_t)random(192), (uint8_t)random(192), (uint8_t)random(192)};
+
+//         switch (random(15)) {
+//         case 0:
+//             backgroundLayer.drawPixel(x0, y0, outlineColor);
+//             // backgroundLayer.readPixel(x0, y0);
+//             break;
+
+//         case 1:
+//             backgroundLayer.drawLine(x0, y0, x1, y1, outlineColor);
+//             break;
+
+//         case 2:
+//             backgroundLayer.drawCircle(x0, y0, radius, outlineColor);
+//             break;
+
+//         case 3:
+//             backgroundLayer.drawTriangle(x0, y0, x1, y1, x2, y2, outlineColor);
+//             break;
+
+//         case 4:
+//             backgroundLayer.drawRectangle(x0, y0, x1, y1, outlineColor);
+//             break;
+
+//         case 5:
+//             backgroundLayer.drawRoundRectangle(x0, y0, x1, y1, radius, outlineColor);
+//             break;
+
+//         case 6:
+//             backgroundLayer.fillCircle(x0, y0, radius, fillColor);
+//             break;
+
+//         case 7:
+//             backgroundLayer.fillTriangle(x0, y0, x1, y1, x2, y2, fillColor);
+//             break;
+
+//         case 8:
+//             backgroundLayer.fillRectangle(x0, y0, x1, y1, fillColor);
+//             break;
+
+//         case 9:
+//             backgroundLayer.fillRoundRectangle(x0, y0, x1, y1, radius, fillColor);
+//             break;
+
+//         case 10:
+//             backgroundLayer.fillCircle(x0, y0, radius, outlineColor, fillColor);
+//             break;
+
+//         case 11:
+//             backgroundLayer.fillTriangle(x0, y0, x1, y1, x2, y2, outlineColor, fillColor);
+//             break;
+
+//         case 12:
+//             backgroundLayer.fillRectangle(x0, y0, x1, y1, outlineColor, fillColor);
+//             break;
+
+//         case 13:
+//             backgroundLayer.fillRoundRectangle(x0, y0, x1, y1, radius, outlineColor, fillColor);
+//             break;
+
+//         case 14:
+//             backgroundLayer.drawEllipse(x0, y0, radius, radius2, outlineColor);
+
+//         default:
+//             break;
+//         }
+//         backgroundLayer.swapBuffers();
+//         //backgroundLayer.fillScreen({0,0,0});
+//         while (millis() < currentMillis + delayBetweenShapes);
+//     }
+// }
